3cb2c89c78df341d0188c3a641163ec8a8afc3e0
aljefferson <aljefferson99501@gmail.com>
2014-06-05 18:10:58 -0700

    Assignment7

---
+++ Assignment7/Assignment7/EmployeeService.svc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Assignment7.DataAccess;
+
+namespace Assignment7
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "EmployeeService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select EmployeeService.svc or EmployeeService.svc.cs at the Solution Explorer and start debugging.
+    public class EmployeeService : IEmployeeService
+    {
+        EmployeeDB db = new EmployeeDB();
+
+        public Employee GetEmployee(int employeeID)
+        {
+            Employee emp = db.GetEmployeeById(employeeID);
+            return emp;
+        }
+
+        public void EditEmployee(Employee employee)
+        {
+            db.EditEmployee(employee);
+        }
+    }
+}
---
+++ Assignment7/Assignment7/IEmployeeService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Assignment7
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IEmployeeService" in both code and config file together.
+    [ServiceContract]
+    public interface IEmployeeService
+    {
+        [OperationContract]
+        Employee GetEmployee(int employeeID);
+
+
+        [OperationContract]
+        void EditEmployee(Employee employee);
+    }
+   
+    
+    [DataContract]
+    public class Employee
+    {
+        [DataMember]
+        public int EmployeeKey { get; set; }
+        [DataMember]
+        public int PersonKey { get; set; }
+        [DataMember]
+        public string LastName { get; set; }
+        [DataMember]
+        public string FirstName { get; set; }
+        [DataMember]
+        public string SocialSecurityNumber { get; set; }
+        [DataMember]
+        public DateTime HireDate { get; set; }
+        [DataMember]
+        public string Status { get; set; }
+        [DataMember]
+        public decimal MonthlySalary { get; set; }
+    }
+}
